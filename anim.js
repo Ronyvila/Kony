02db3c80bc7690d30fad1bd07a5d4a7b0fcf0b8c
Ronyvila <rony.vg18@gmail.com>
2024-10-16 20:04:44 -0500

    Add files via upload

---
+++ anim.js
@@ -0,0 +1,54 @@
+// Sincronizar las letras con la canción
+var audio = document.querySelector("audio");
+var lyrics = document.querySelector("#lyrics");
+
+// Array de objetos que contiene cada línea y su tiempo de aparición en segundos
+var lyricsData = [
+
+  { text: "Tú me hablabas del amor, de eso tan bello", time: 16 },
+  { text: "Te faltó por demostrar que eras capaz de vivir aquello", time: 22 },
+  { text: "Pero no te olvides por favor que", time: 28 },
+  { text: "Sigo aquí ya no quiero sufrir más por ti, mi amor", time: 35 },
+  { text: "Con el tiempo decidí que es un recuerdo", time: 43 },
+  { text: "Todo aquello que viví y me mató por dentro", time: 53 },
+  { text: "Todo aquello que viví y me mató por dentro", time: 58 },
+  { text: "Love.", time: 140 },
+];
+
+// Animar las letras
+function updateLyrics() {
+  var time = Math.floor(audio.currentTime);
+  var currentLine = lyricsData.find(
+    (line) => time >= line.time && time < line.time + 6
+  );
+
+  if (currentLine) {
+    // Calcula la opacidad basada en el tiempo en la línea actual
+    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
+    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
+
+    // Aplica el efecto de aparición
+    lyrics.style.opacity = opacity;
+    lyrics.innerHTML = currentLine.text;
+  } else {
+    // Restablece la opacidad y el contenido si no hay una línea actual
+    lyrics.style.opacity = 0;
+    lyrics.innerHTML = "";
+  }
+}
+
+setInterval(updateLyrics, 1000);
+
+//funcion titulo
+// Función para ocultar el título después de 216 segundos
+function ocultarTitulo() {
+  var titulo = document.querySelector(".titulo");
+  titulo.style.animation =
+    "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
+  setTimeout(function () {
+    titulo.style.display = "none";
+  }, 3000); // Espera 3 segundos antes de ocultar completamente
+}
+
+// Llama a la función después de 216 segundos (216,000 milisegundos)
+setTimeout(ocultarTitulo, 216000);
